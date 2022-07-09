273c1e39226fd0819d0134a35cc39b91f8216ed4
Nuha Qadir <nuhaqadir@fb.com>
2022-07-09 14:29:56 -0700

    spotify backend works

--- backend/models/playlist.js
+++ backend/models/playlist.js
@@ -1,35 +1,85 @@
 const { BadRequestError, NotFoundError } = require("../utils/errors")
+const axios = require('axios');
+const { response } = require("../app");
+
 
 class Playlist {
     constructor() {
         this.super();
     }
-    static search(term, token) {
-        //same as routes
-        try {
-            var options = {
-              url: `https://api.spotify.com/v1/search?q=${req.params.term}&type=playlist`,
-              headers: { 'Authorization': 'Bearer ' + req.body.token},
-              json: true
-            };
-        
-            request.get(options, function(error, response, body) {
-              res.status(200).json({body})
-            });
-        
-          } catch(err) {
-            next(err)
-          }
+
+    /**
+     * @param term is the search term 
+     * @param token is access token for Spotify API 
+     * @returns a list of the 5 top playlists using the provided search term
+     */
+    static async search(term, token) {
+        const url = `https://api.spotify.com/v1/search?q=${term}&type=playlist&limit=5`
+        const response = await axios.get(url, {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          });     
+        //return response.data.playlists.items     
+          return this.getSongs(response.data.playlists.items, token)
     }
 
-    //from a list of playlists, choose 5 random songs
-    static getSongs(playlistList) {
+    /**
+     * 
+     * @param playlistList is a list of 5 playlists from the function above
+     * @param token is access token for Spotify API 
+     * @returns 5 random songs from the given playlists
+     */
+    static async getSongs(playlistList, token) {
+        const urls = playlistList.map(e => {
+            return e.tracks.href
+        })
 
+        let songs = await this.getSongsHelper(urls[0], token)
+        songs = songs.concat(await this.getSongsHelper(urls[1], token))
+        songs = songs.concat(await this.getSongsHelper(urls[2], token))
+        //choose 5 random indices: 
+        let arr= []
+        for (let i =0; i < 5; i++) {
+            let index = (Math.random() * songs.length-1).toFixed(0)
+            while (arr.includes(index)){
+                index = (Math.random() * songs.length-1).toFixed(0)
+            }
+            arr.push(index)
+        }
+        let seedSongs = []
+        arr.forEach(i => {
+            seedSongs.push(songs[i])
+        })
+        //  return seedSongs[0]
+        return this.getRec(seedSongs, token)
+    }
+    static async getSongsHelper(url, token) {
+        const response = await axios.get(url, {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          });
+        return response.data.items.slice(0, 8)
     }
 
     //from 5 songs, get recommendations
-    static getRec(songArr) {
+    static async getRec(seedSongs, token) {
+        let str = seedSongs[0].track.id
+        for (let i=1; i < seedSongs.length-1; i++) {
+            str.concat(`,${seedSongs[i].track.id}`)
+
+        }
+
+        const response = await axios.get(`https://api.spotify.com/v1/recommendations?min_popularity=50&limit=8&seed_tracks=${str}`, {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          });
+          return response.data
 
     }
 
 }
+
+module.exports = Playlist
--- backend/routes/playlist.js
+++ backend/routes/playlist.js
@@ -2,26 +2,31 @@ const express = require("express")
 const router = express.Router()
 var request = require('request'); // "Request" library
 var cors = require('cors');
+const Playlist = require("../models/playlist")
+
 
 router.use(cors())
 
 //front end sends search term, back end does all spotify calls
 router.get('/search/:term', async (req, res, next) => {
+    const list = await Playlist.search(req.params.term, req.body.token)
+    res.status(201).send(list)
+    
     //call 3 functions in models
-    try {
-      var options = {
-        url: `https://api.spotify.com/v1/search?q=${req.params.term}&type=playlist`,
-        headers: { 'Authorization': 'Bearer ' + req.body.token},
-        json: true
-      };
+    // try {
+    //   var options = {
+    //     url: `https://api.spotify.com/v1/search?q=${req.params.term}&type=playlist`,
+    //     headers: { 'Authorization': 'Bearer ' + req.body.token},
+    //     json: true
+    //   };
   
-      request.get(options, function(error, response, body) {
-        res.status(200).json({body})
-      });
+    //   request.get(options, function(error, response, body) {
+    //     res.status(200).json({body})
+    //   });
   
-    } catch(err) {
-      next(err)
-    }
+    // } catch(err) {
+    //   next(err)
+    // }
   })
 
 
